60004074be771a6914c977563546ed9732eaeaec
João Flavo Costa Teixeira Filho <72360863+Joaoflavo@users.noreply.github.com>
2020-10-13 21:32:17 -0300

    Add files via upload
    Treino criar formulário simples em python

---
+++ aula_5_formulario simples.py
@@ -0,0 +1,23 @@
+print('Formulario simples', 'Digite seus dados abaixo:')
+nome = input('Digite seu nome:')
+cpf = input('Digite seu CPF:')
+endereco = input('Digite seu endereço:')
+idade = input('Digite sua idade:')
+altura = input('Digite sua altura:')
+telefone = input('Digite seu telefone:')
+print('Seu nome nome é:', nome)
+print('Seu cpf é:', cpf)
+print('seu endereço é:', endereco)
+print('Sua idade é:', idade)
+print('Sua altura é:', altura)
+print('Seu telefone é:', telefone)
+
+
+#nome = 'Guilherme'
+#idade =  26
+#altura = 1.78
+#print (nome, 'tem', idade, 'anos e', altura, 'de altura')
+
+
+# print('test \n test após quebra de linha')
+# print('test \t test após tablet')
